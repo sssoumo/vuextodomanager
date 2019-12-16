d02c59990637559f4a08c5b651a5057137774f9f
Saadman Soumique <sssoumo@live.com>
2019-12-16 16:44:00 +0600

    removed eslint and added add todo component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    <Todos/>
+    <div class="container">
+      <AddTodo/>
+      <Todos/>
+    </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Todos
+  import Todos from './components/Todos.vue'
+  import AddTodo from './components/AddTodo.vue'
+  export default {
+    name: 'app',
+    components: {
+      Todos,
+      AddTodo
+    }
   }
-}
 </script>
 
 <style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h3>Add todo</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddTodo'
+}
+</script>
