efe1f60e4c22d80d7317e58c12f216dbef537a47
Saadman Soumique <sssoumo@live.com>
2019-12-16 13:51:17 +0600

    vuex added

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,12 +2,19 @@
     <div>
         <div class="container">
             <h3>Todos</h3>
+                <div class="todos">
+                    <div v-for="todo in allTodos" :key="todo.id" class="todo">
+                        {{ todo.title }}
+                    </div>
+                </div>
         </div>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-    name: "Todos"
+    name: "Todos",
+    computed: mapGetters(['allTodos'])
 }
 </script>
