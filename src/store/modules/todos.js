e34f7886ed87a9713ee9d33629e1484b49813f89
Saadman Soumique <sssoumo@live.com>
2019-12-16 19:10:39 +0600

    add funtionality for todo list implemented

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -13,11 +13,18 @@ const actions = {
         const response= await Axios.get('https://jsonplaceholder.typicode.com/todos');
 
         commit('setTodos',response.data);
+    },
+
+    async addTodo({commit}, title){
+        const response= await Axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed: false});
+
+        commit('newTodo',response.data);
     }
 };
 
 const mutations = {
-    setTodos: (state, todos) => (state.todos = todos)
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => (state.todos.unshift(todo))
 };
 
 export default {
